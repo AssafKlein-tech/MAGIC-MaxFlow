eaf25651c77f90e357871740d9937b40d5503629
Assaf Klein <assafklein@campus.technion.ac.il>
2023-01-23 12:03:09 +0200

    parallel computing

--- Gridcut/examples/ex2-segmentation/ex2-segmentation.cpp
+++ Gridcut/examples/ex2-segmentation/ex2-segmentation.cpp
@@ -25,11 +25,54 @@ using namespace std;
 // (v^2e c) 
 #define K 1000
 #define SIGMA2 0.012f
-#define WEIGHT(A) (short)(1+K*std::exp((-(A)*(A)/SIGMA2)))
-#define P  (int)2
+#define WEIGHT(A) (short)(10+K*std::exp((-(A)*(A)/SIGMA2)))
+#define P  (int)1
 
 #define RED  RGB(1,0,0)
 #define BLUE RGB(0,0,1)
+
+float get_dist(float p1, float p2)
+{
+    return pow(p1-p2, 2);
+}
+
+short weight(float dist, float sigma)
+{
+    return (short)(10+K*std::exp((-(dist)/(2*sigma))));
+}
+
+template <typename T>
+float get_sigma(const string image_path)
+{
+    const Image<T> image = imread<T>(image_path);
+    int w = image.width();
+    int h = image.height();
+    unsigned long edge_num = 0;
+    float diff;
+    float temp_avg = 0;
+    for (int y = 0; y < h; y++)
+    {
+        for (int x = 0; x < w; x++)
+        {
+            if (x<w-1)
+            {
+                diff = get_dist(image(x,y),image(x+1,y));
+                temp_avg += (diff - temp_avg)/(++edge_num);
+                //cout << "diff " << diff << endl;
+
+            }
+
+            if (y< h-1)
+            {
+                diff = get_dist(image(x,y),image(x,y+1));
+                temp_avg += (diff - temp_avg)/(++edge_num);
+            }
+        }
+    }
+    return temp_avg;
+}
+
+
 
 int main(int argc,char** argv)
 {
@@ -46,13 +89,21 @@ int main(int argc,char** argv)
   const Image<float> image = imread<float>(image_path);
   const Image<RGB> scribbles = imread<RGB>(scribbles_path);
 
+  const float sigma = get_sigma<float>(image_path);
+
   int width  = scribbles.width();
   int height = scribbles.height();
 
 
   cout << "width: " << width << " height: " << height << endl;
   
-  vector<Grid> grids = vector<Grid>(P,Grid(width,height));
+  Grid* grid;
+  vector<Grid*> grids;
+  for (int i = 0; i <P; i++)
+  {
+    grid = new Grid(width, height);
+    grids.push_back(grid);
+  }
 
   short cap;
 
@@ -60,64 +111,46 @@ int main(int argc,char** argv)
   {
     for (int x=0;x<width;x++)
     {
-      cout << "fgsg" <<endl;
-      for (int i=0;i<P; ++i)
+
+      for (auto it : grids)
       {
-        cout << x <<"," << y << "   " << grids[i].node_id(x  ,y) <<endl;
-        grids[i].set_terminal_cap(grids[i].node_id(x,y),
+        it->set_terminal_cap(it->node_id(x,y),
                              scribbles(x,y)==BLUE ? K : 0,
                              scribbles(x,y)==RED  ? K : 0);
       }
-      cout << "2" <<endl;
 
       if (x<width-1)
       {
-        
-
-        for (int i=0;i<P; ++i)
+        cap = weight(get_dist(image(x,y),image(x+1,y)),sigma);
+        for (auto it : grids)
         {
-          cap = WEIGHT(image(x,y)-image(x+1,y));
-          cout << x <<"," << y << "   " << grids[i].node_id(x  ,y) <<endl;
-          grids[i].set_neighbor_cap(grids[i].node_id(x  ,y),+1,0,cap);
-          cout << i <<endl;
-          grids[i].set_neighbor_cap(grids[i].node_id(x+1,y),-1,0,cap);
+          it->set_neighbor_cap(it->node_id(x  ,y),+1,0,cap);
+          it->set_neighbor_cap(it->node_id(x+1,y),-1,0,cap);
         }
       }
-      cout << "3" <<endl;
       if (y<height-1)
       {
-        cap = WEIGHT(image(x,y)-image(x,y+1));
-        for (int i=0;i<P; ++i)
+        cap = weight(get_dist(image(x,y),image(x,y+1)),sigma);
+        for (auto it : grids)
         {
-          grids[i].set_neighbor_cap(grids[i].node_id(x,y  ),0,+1,cap);
-          grids[i].set_neighbor_cap(grids[i].node_id(x,y+1),0,-1,cap);
+          it->set_neighbor_cap(it->node_id(x,y  ),0,+1,cap);
+          it->set_neighbor_cap(it->node_id(x,y+1),0,-1,cap);
         }
       }
-      cout << "4" <<endl;
     }
   }
 
-  
-
-  grids[0].compute_maxflow();
-  //cout << grids[0].get_flow() << endl;
-  grids[1].compute_maxflow();
-  cout << grids[1].get_flow() << endl;
   std::vector<std::thread> grid_threads;
-  auto start = sc.now();
-  for (int i=0;i<P; ++i)
-  {
-    grids[i].compute_maxflow();
-    cout << grids[i].get_flow() << endl;
-  }
-  /**
-  //creat P threads
-  for (int i=0;i<P; ++i)
-  {
-     grid_threads.push_back(std::thread(&Grid::compute_maxflow,&grids[i]));
-  }
-  cout<< grid_threads.size() << endl;
   int count = 0;
+
+
+  auto start = sc.now();
+  //creat P threads
+  for (auto it : grids)
+  {
+    grid_threads.push_back(std::thread(&Grid::compute_maxflow,it));
+  }
+
   //join Pthreads
   while (count < P)
   {
@@ -128,31 +161,15 @@ int main(int argc,char** argv)
         count++;
       }
   }
-  **/
+
   auto end = sc.now();
   double time_span = static_cast<chrono::duration<double>>(end - start).count();
-
-
-/**
-  Image<RGB> output(width,height);
-  int cutsize = 0;
-
-  for (int y=0;y<height;y++)
-  {
-    for (int x=0;x<width;x++)
-    {
-      output(x,y) = image(x,y)*(grid->get_segment(grid->node_id(x,y)) ? RED : BLUE);
-      if (!grid->get_segment(grid->node_id(x,y)))
-         cutsize+=1;
-    }
-  }
-
-  int maxflow = grid->get_flow();
-  **/
-
-  //imwrite(output,out_path);  
+  
 
   printf(" The time is %f\n",time_span);
-  
+  for (auto it : grids)
+  {
+    delete it;
+  }
   return 0;
 }
--- Gridcut/segmentation/timer.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIMER_H_
-#define TIMER_H_
-
-void timerReset();
-double timerGet();
-
-#endif
